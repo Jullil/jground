120e679a34a9b844645f07708de7204ebc0b304e
Julia Loykova <julinary@gmail.com>
2014-04-02 00:54:14 +0400

    Init

--- jground/src/org/jground/Main.java
+++ jground/src/org/jground/Main.java
@@ -1,7 +1,12 @@
 package org.jground;
 
+import org.apache.log4j.Logger;
+
 public class Main {
+    static Logger logger = Logger.getRootLogger();
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        logger.debug("Hello World!!!!!!!!!!!!!!!!!");
     }
 }
