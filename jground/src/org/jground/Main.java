0efc12cb29bbee406f3054e13b25045f04fca763
Julia Loykova <julinary@gmail.com>
2014-04-02 01:11:41 +0400

    Init

--- jground/src/org/jground/Main.java
+++ jground/src/org/jground/Main.java
@@ -1,12 +1,22 @@
 package org.jground;
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 public class Main {
     static Logger logger = Logger.getRootLogger();
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            PropertyConfigurator.configure("conf/log4j.properties");
+            logger.info("Starting the system.");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+
+        System.out.println("Hellod world!");
         logger.debug("Hello World!!!!!!!!!!!!!!!!!");
     }
 }
